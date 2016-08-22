fa686d9ab588acfb5f859140e72d07d65e0577ef
Michele Giovanni Curletta <michele.curletta@icloud.com>
2016-08-22 15:41:45 +0200

    style fix
    style fix

--- src/Spatie/Geocoder/GeocoderServiceProvider.php
+++ src/Spatie/Geocoder/GeocoderServiceProvider.php
@@ -6,15 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class GeocoderServiceProvider extends ServiceProvider
 {
-	public function boot()
-	{
-		$this->publishes([__DIR__.'/../config/geocoder.php' => config_path('geocoder.php'),], 'config');
-	}
+    public function boot()
+    {
+        $this->publishes([__DIR__.'/../config/geocoder.php' => config_path('geocoder.php'),], 'config');
+    }
 
     public function register()
     {
-    	$this->mergeConfigFrom(__DIR__.'/../config/geocoder.php', 'geocoder');
-    	
+        $this->mergeConfigFrom(__DIR__.'/../config/geocoder.php', 'geocoder');
+
         $this->app->bind(
             'geocoder',
             'Spatie\Geocoder\Google\Geocoder'
