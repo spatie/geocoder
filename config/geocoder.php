6dd8fa6b943bacad1ffb9953cc93f96ca27a5eb8
Freek Van der Herten <freek@spatie.be>
2017-10-24 08:52:12 +0000

    Apply fixes from StyleCI

--- config/geocoder.php
+++ config/geocoder.php
@@ -7,7 +7,6 @@ return [
      */
     'key' => env('GOOGLE_MAPS_GEOCODING_API_KEY', ''),
 
-
     /*
      * The language param used to set response translations for textual data.
      * More info: https://developers.google.com/maps/faq#languagesupport
